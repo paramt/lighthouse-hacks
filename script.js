b18d89beaeb60167ae0fc8ce563d19f04c99e6e3
Param Thakkar <pht0104@gmail.com>
2019-10-21 16:48:23 -0400

    Clean up code

--- script.js
+++ script.js
@@ -1,19 +1,9 @@
-window.setTimeout(function(){
-    //document.getElementById("arrow").style.opacity = "1";
-    //document.getElementById('arrow').classList = 'animated bounce';
-  }, 800)
+window.addEventListener("scroll", function(){
+  var scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop;
 
-  window.addEventListener("scroll", function(){
-    var scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop;
+  if(scrollTop !== 0){
 
-    if(scrollTop !== 0){
-      //document.getElementById('arrow').classList = 'animated fadeOutUp';
-      document.body.style.backgroundColor = "#6600cc";
-      //document.getElementById('main').classList = 'main-streched';
-    } else {
-
-      //document.getElementById('arrow').classList = 'animated bounce';
-      //document.getElementById('main').classList = 'main';
-      document.body.style.backgroundColor = "white";
-    }
-  })
+  } else {
+    document.body.style.backgroundColor = "white";
+  }
+})
