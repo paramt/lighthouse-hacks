4f97924117ff8e12e8f6e324c2a0c185ae168f05
Param Thakkar <pht0104@gmail.com>
2019-10-21 16:49:12 -0400

    Fix JavaScript

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.addEventListener("scroll", function(){
   var scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop;
 
   if(scrollTop !== 0){
-
+    document.body.style.backgroundColor = "#6600cc";
   } else {
     document.body.style.backgroundColor = "white";
   }
