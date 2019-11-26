1e4ee3fdfbf96f69d2c1c2e28e647d07a2c810da
Param Thakkar <pht0104@gmail.com>
2019-11-26 16:31:22 -0500

    Create FAQ section

--- script.js
+++ script.js
@@ -8,9 +8,24 @@ window.onload = function(){
 window.addEventListener("scroll", function(){
     var scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop;
 
-    if(scrollTop !== 0){
-      document.getElementById("arrow").classList = "animated fadeOutUp";
-    } else {
+    if(scrollTop == 0){
       document.getElementById("arrow").classList = "animated bounce";
     }
 }, false);
+
+$(document).ready(function() {
+  //toggle the component with class slideout
+  $(".box").click(function() {
+    if ($('.slideout').is(':visible')) {
+      $(".slideout").slideUp(300);
+      $(".plusminus").text('+');
+    }
+    if ($(this).next(".slideout").is(':visible')) {
+      $(this).next(".slideout").slideUp(300);
+      $(this).find(".plusminus").text('+');
+    } else {
+      $(this).next(".slideout").slideDown(300);
+      $(this).find(".plusminus").text('–');
+    }
+  });
+});
